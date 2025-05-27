87004b73e07e5733939d8dc5138c494fa5210614
riices <150382838+riicess@users.noreply.github.com>
2025-05-27 18:38:02 +0300

    Update timesync.bat

--- timesync.bat
+++ timesync.bat
@@ -2,7 +2,7 @@
 net session >nul 2>&1
 if %errorLevel% neq 0 (
     echo Need admin rights to update how your computer gets the time
-    echo Please approve the security prompt that will pops up
+    echo Please approve the security prompt that will pop up
     powershell -Command "Start-Process '%~f0' -Verb runAs"
     exit /b
 )
@@ -10,16 +10,17 @@ if %errorLevel% neq 0 (
 sc config w32time start= auto >nul 2>&1
 net start w32time >nul 2>&1
 
-reg add "HKLM\SYSTEM\CurrentControlSet\Services\W32Time\TimeProviders\NtpClient" /v Enabled /t REG_DWORD /d 1 /f >nul 2>&2
-reg add "HKLM\SYSTEM\CurrentControlSet\Services\W32Time\TimeProviders\NtpClient" /v SpecialPollInterval /t REG_DWORD /d 3600 /f >nul 2>&2
-reg add "HKLM\SYSTEM\CurrentControlSet\Services\W32Time\TimeProviders\NtpClient" /v NtpServer /t REG_SZ /d time.windows.com,0x9 /f >nul 2>&2
-reg add "HKLM\SYSTEM\CurrentControlSet\Services\W32Time\Parameters" /v Type /t REG_SZ /d NTP /f >nul 2>&2
-reg add "HKLM\SYSTEM\CurrentControlSet\Services\W32Time\Config" /v AnnounceFlags /t REG_DWORD /d 5 /f >nul 2>&2
+reg add "HKLM\SYSTEM\CurrentControlSet\Services\W32Time\TimeProviders\NtpClient" /v Enabled /t REG_DWORD /d 1 /f >nul 2>&1
+reg add "HKLM\SYSTEM\CurrentControlSet\Services\W32Time\TimeProviders\NtpClient" /v SpecialPollInterval /t REG_DWORD /d 3600 /f >nul 2>&1
+reg add "HKLM\SYSTEM\CurrentControlSet\Services\W32Time\TimeProviders\NtpClient" /v NtpServer /t REG_SZ /d time.windows.com,0x9 /f >nul 2>&1
+reg add "HKLM\SYSTEM\CurrentControlSet\Services\W32Time\Parameters" /v Type /t REG_SZ /d NTP /f >nul 2>&1
+reg add "HKLM\SYSTEM\CurrentControlSet\Services\W32Time\Config" /v AnnounceFlags /t REG_DWORD /d 5 /f >nul 2>&1
 
 net stop w32time >nul 2>&1
 net start w32time >nul 2>&1
-w32tm /config /update
-w32tm /resync
+
+w32tm /config /update >nul 2>&1
+w32tm /resync >nul 2>&1
 
 echo.
 echo Operation complete.
@@ -37,4 +38,4 @@ if /i "%RestartChoice%"=="Y" (
     goto RestartPrompt
 )
 
-exit /b  
+exit /b
